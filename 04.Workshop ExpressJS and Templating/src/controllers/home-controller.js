e366de89cb71916d37137e25a7370c8fc19b313d
bozhidar <bozhidarzagorov@gmail.com>
2025-01-15 22:48:40 +0200

    Workshop Movie Magic #1

---
+++ 04.Workshop ExpressJS and Templating/src/controllers/home-controller.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import movieService from '../services/movie-service.js';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    const movies = movieService.getAll();
+
+    res.render('home', { movies });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+export default router;
